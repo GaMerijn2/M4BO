e1219b73b53c1df6e336525d9e5443718f8d8076
merijn florie <merijn@florie.nl>
2023-05-10 11:38:08 +0200

    movement script organized

--- DDIS/Assets/Scripts/PlayerBehaviour.cs
+++ DDIS/Assets/Scripts/PlayerBehaviour.cs
@@ -1,43 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+//using UnityEngine.UIElements;
 
 public class PlayerBehaviour : MonoBehaviour
 {
-
     public CharacterController controller;
     public Transform cam;
 
+    //movement stuff
     public float speed = 6f;
+    public float rotSpeed = 5f;
     public float gravity = -9.81f;
     public float jumpHeight = 3f;
 
-    public Transform groundCheck;
-    public float groundDistance = 0.4f;
-    public LayerMask groundMask;
-
-    Vector3 velocity;
-    bool isGrounded;
-
+    //movement rotation
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
 
+    //groundcheck stuff
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    Vector3 velocity;
+    bool isGrounded;
+
+    //displays HP
     public TextMeshProUGUI healthDisplay;
 
-    //public GameObject maincamera;
-    //public GameObject aimcamera;
+    //The circle that tells you where u looking
+    [SerializeField] Transform aimPos;
+    [SerializeField] float aimSmoothSpeed = 5;
+    [SerializeField] LayerMask aimMask;
 
-
+    //camers for ADS, not working yet
+    public GameObject maincamera;
+    public GameObject aimcamera;
 
     // Update is called once per frame
     void Update()
     {
-        float currentHealth = GameManager.gameManager.playerHealth.Health;
-        float currentMaxHealth = GameManager.gameManager.playerHealth.MaxHealth;
-
         Cursor.lockState = CursorLockMode.Locked;
-
+        Hp();
+        PlayerMovement();
+        MouseMovement();
 
         if (Input.GetKey(KeyCode.LeftControl))
         {
@@ -48,24 +57,11 @@ public class PlayerBehaviour : MonoBehaviour
         {
             Application.Quit();
         }
-
-
-        /*
-        if(Input.GetKey(KeyCode.K)) 
-        {
-            maincamera.SetActive(false);
-            aimcamera.SetActive(true);
-        }
-        else
-        {
-            maincamera.SetActive(true);
-            aimcamera.SetActive(false);
-        }
-        */
-
-
-
-
+    }
+    private void Hp()
+    {
+        float currentHealth = GameManager.gameManager.playerHealth.Health;
+        float currentMaxHealth = GameManager.gameManager.playerHealth.MaxHealth;
 
         if (healthDisplay != null)
         {
@@ -82,10 +78,22 @@ public class PlayerBehaviour : MonoBehaviour
             PlayerHeal(25);
             Debug.Log("PlayerHealth: " + GameManager.gameManager.playerHealth.Health.ToString());
         }
+    }
+    private void PlayerTakeDmg(int dmg)
+    {
+        GameManager.gameManager.playerHealth.DmgUnit(dmg);
+    }
+    private void PlayerHeal(int healing)
+    {
+        GameManager.gameManager.playerHealth.HealUnit(healing);
+    }
+    public void DestroyUnit()
+    {
+        Destroy(gameObject);
 
-
-
-
+    }
+    private void PlayerMovement()
+    {
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
         if (isGrounded && velocity.y < 0)
@@ -118,18 +126,36 @@ public class PlayerBehaviour : MonoBehaviour
             controller.Move(moveDir.normalized * speed * Time.deltaTime);
         }
     }
-    private void PlayerTakeDmg(int dmg)
+    private void MouseMovement()
     {
-        GameManager.gameManager.playerHealth.DmgUnit(dmg);
-    }
-    private void PlayerHeal(int healing)
-    {
-        GameManager.gameManager.playerHealth.HealUnit(healing);
-    }
-    public void DestroyUnit()
-    {
-        Destroy(gameObject);
+        Vector2 screenCentre = new Vector2(Screen.width / 2, Screen.height / 2);
+        Ray ray = Camera.main.ScreenPointToRay(screenCentre);
 
+        if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, aimMask))
+        {
+            aimPos.position = Vector3.Lerp(aimPos.position, hit.point, aimSmoothSpeed * Time.deltaTime);
+        }
+
+        /*
+        if (Input.GetMouseButton(1))
+        {
+            Debug.Log("RMB");
+            transform.rotation = cam.transform.rotation;
+            maincamera.SetActive(false);
+            aimcamera.SetActive(true);
+
+            
+            float mouseXValue = Input.GetAxis("Mouse X");
+            float mouseYValue = Input.GetAxis("Mouse Y");
+            transform.rotation *= Quaternion.Euler(mouseXValue * rotSpeed * Vector3.up);
+            
+        }
+        else
+        {
+            maincamera.SetActive(true);
+            aimcamera.SetActive(false);
+        }
+            */
     }
 
 }
