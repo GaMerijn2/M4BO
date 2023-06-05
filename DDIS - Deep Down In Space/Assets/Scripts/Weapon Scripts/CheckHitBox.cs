3a734e587c1b5a0aa8bae3d4883a87f77b4c1e2c
merijn florie <merijn@florie.nl>
2023-06-05 10:18:00 +0200

    Enemy Hp Fixed

--- DDIS - Deep Down In Space/Assets/Scripts/Weapon Scripts/CheckHitBox.cs
+++ DDIS - Deep Down In Space/Assets/Scripts/Weapon Scripts/CheckHitBox.cs
@@ -7,9 +7,10 @@ public class CheckHitBox : MonoBehaviour
     private void OnTriggerEnter(Collider col)
     {
         col.gameObject.CompareTag("Enemy");
-        Debug.Log(col.gameObject.name);
+
         if (col.gameObject.name == "Enemy")
         {
+            Debug.Log(col.gameObject.name);
             col.gameObject.GetComponent<EnemyAiTutorial>().EnemyTakeDmg(20);
             Debug.Log(col.gameObject.GetComponent<EnemyAiTutorial>().health);
         }
